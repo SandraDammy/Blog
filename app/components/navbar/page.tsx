785e5075fe829319e64f35e0363870d72ffcaf90
Oluwadamilola Odujirin <oluwadamilolaod@HA-SHEM.com>
2024-02-06 13:00:46 +0100

    navbar_setup

--- app/components/navbar/page.tsx
+++ app/components/navbar/page.tsx
@@ -1,12 +1,12 @@
-import Link from 'next/link'
 import React from 'react'
+import Links from './links/Links'
 
 const Navbar = () => {
   return (
     <div>
       <div>Logo</div>
       <div>
-        
+        <Links/>
       </div>
     </div>
   )
