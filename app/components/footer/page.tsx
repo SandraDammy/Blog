4ceffad64e523716aa9d66b92ff5b8d19af4e858
Oluwadamilola Odujirin <oluwadamilolaod@HA-SHEM.com>
2024-02-06 12:59:33 +0100

    page_created

---
+++ app/components/footer/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>Footerpage</div>
+  )
+}
+
+export default Footer
---
+++ app/components/navbar/page.tsx
@@ -0,0 +1,15 @@
+import Link from 'next/link'
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      <div>Logo</div>
+      <div>
+        
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
